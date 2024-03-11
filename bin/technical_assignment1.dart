fcf4c0ffef8b37077ee2798748bb7db49ef81cfd
Collins Wegulo <wegulocollins77@gmail.com>
2024-03-11 20:33:22 +0300

    first commit

---
+++ bin/technical_assignment1.dart
@@ -0,0 +1,30 @@
+void main() {
+  // string 
+  String name = "Collins";
+  print("Name: $name");
+
+  // double (double)
+  double height = 6.1;
+  print("Height: $height");
+
+  // int (int)
+  int age = 23;
+  print("Age: $age");
+
+  // Boolean data type
+  bool student = true;
+  print("Is student: $student");
+
+  // List data type
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print("Numbers: $numbers");
+
+  // Map data type
+  Map<String, dynamic> person = {
+    'name': 'Collins',
+    'age': 23,
+    'student': true
+  };
+  print("Person: $person");
+
+}
---
+++ test/technical_assignment1_test.dart
@@ -0,0 +1,8 @@
+import 'package:technical_assignment1/technical_assignment1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
